afbc167cd023a4f76c5f822442bcea5c9acc1013
Dhyey Vithalani <dhyey97@gmail.com>
2023-03-08 18:33:46 +0530

    Updates

---
+++ text.js
@@ -0,0 +1,18 @@
+
+// let string1 = 'the quick brown fox'
+// let arr = string1.split(" ") 
+// var obj = ""
+// for (var i = 0; i < arr.length; i++) {
+//     arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+// }
+// const str2 = arr.join(" ")
+// console.log(str2)
+
+
+var arr = ["the","quick"]
+
+arr[0] = ""
+console.log(arr)
+
+// arr ==> Arr
+// quick ==> Quick
